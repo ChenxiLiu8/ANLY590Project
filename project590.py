9aac8df8a3249ff09836075b6ab0e8f20623c9ad
Chenxi Liu <54824696+ChenxiLiu8@users.noreply.github.com>
2020-11-22 17:24:26 -0500

    Update project590.py

--- project590.py
+++ project590.py
@@ -10,14 +10,21 @@ def create_input(sample_dict):
     return_list = list()
     context = sample_dict['context']
     for i in range(len(sample_dict['qas'])):
-        question = sample_dict['qas'][i]['question']    
+        question = sample_dict['qas'][i]['question']
+        if sample_dict['qas'][i]['is_impossible'] == False:
+            if len(sample_dict['qas'][i]['answers']) > 1:
+                print('haha multiple!')
+            answer = sample_dict['qas'][i]['answers'][0]['text']   
+        else: 
+            answer = ''
         return_str = str(context) + str(question)
-        return_list.append(return_str)
+        return_tuple = (return_str,answer)
+        return_list.append(return_tuple)
     return return_list
+
 
 total_list = list()
 for i in range(len(data['data'])):
     for j in range(len(data['data'][i]['paragraphs'])):
         list_to_append = create_input(data['data'][i]['paragraphs'][j])
         total_list.extend(list_to_append)
-print(total_list[0])
