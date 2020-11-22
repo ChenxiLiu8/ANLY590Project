840a660644c837133ef26f73c833bd7cd10a41d0
Chenxi Liu <54824696+ChenxiLiu8@users.noreply.github.com>
2020-11-22 17:12:54 -0500

    Add files via upload

---
+++ project590.py
@@ -0,0 +1,23 @@
+import json 
+
+with open('training.json') as f :
+    data = json.load(f)
+
+
+
+#sample_dict = data['data'][0]['paragraphs'][0]
+def create_input(sample_dict):
+    return_list = list()
+    context = sample_dict['context']
+    for i in range(len(sample_dict['qas'])):
+        question = sample_dict['qas'][i]['question']    
+        return_str = str(context) + str(question)
+        return_list.append(return_str)
+    return return_list
+
+total_list = list()
+for i in range(len(data['data'])):
+    for j in range(len(data['data'][i]['paragraphs'])):
+        list_to_append = create_input(data['data'][i]['paragraphs'][j])
+        total_list.extend(list_to_append)
+print(total_list[0])
